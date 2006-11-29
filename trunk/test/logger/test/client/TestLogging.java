9edea7471c4ccb5bb03dc1682dedf81c1e0ac6a8
sandymac <sandymac@a255dbe3-2823-0410-b84b-5b1f672b3605>
2006-11-29 22:13:16 +0000

    minor change

--- trunk/test/logger/test/client/TestLogging.java
+++ trunk/test/logger/test/client/TestLogging.java
@@ -6,6 +6,7 @@ import com.google.gwt.user.client.ui.*;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.logging.Handler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,18 +17,45 @@ import java.util.logging.Level;
  */
 public class TestLogging implements EntryPoint {
 
-    private static final Logger logger = Logger.getLogger(GWT.getTypeName(TestLogging.class));
+    private static final Logger loggerP = Logger.getLogger("logger.test");
+    private final Logger logger = Logger.getLogger(GWT.getTypeName(this));
 
     public void onModuleLoad() {
 
         logger.finest("Finest");
-        logger.finer("Finer");
-        logger.fine("Fine");
+        //logger.finer("Finer");
+        //logger.fine("Fine");
         logger.config("Config");
         logger.info("Info");
-        logger.warning("Warning");
-        logger.severe("Severe");
+        //logger.warning("Warning");
+        //logger.severe("Severe");
 
-        RootPanel.get().add(new Label("Loaded!"));
+        loggerP.warning("loggerP");
+
+        RootPanel.get().add(new Label("Loaded: " + logger.getName() + " : " + logger.getLevel()));
+        RootPanel.get().add(new Label("Loaded: " + loggerP.getName()));
+
+        Logger.getLogger("").getHandlers()[0].setLevel(Level.ALL);
+
+        logger.finest("Finest before");
+        logger.setLevel(Level.FINEST);
+        RootPanel.get().add(new Label("FINER loggable: " + logger.isLoggable(Level.FINER)));
+        logger.finest("Finest after");
+
+
+        appendLoggers(logger);
+    }
+
+    private void appendLoggers(Logger logger) {
+        if (logger == null) return;
+        RootPanel.get().add(new Label("Logger: " + logger.getName() + " : " + logger.getLevel()));
+
+        Handler[] handlers = logger.getHandlers();
+        for (int i=0; i < handlers.length; i++) {
+            Handler handler = handlers[i];
+            RootPanel.get().add(new Label("Handler: " + GWT.getTypeName(handler) + " : " + handler.getLevel()));
+        }
+
+        appendLoggers(logger.getParent());
     }
 }
